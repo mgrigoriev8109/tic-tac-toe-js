ef0140c57ddb69d153984876bd7815a3deb7d18a
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2022-06-09 10:40:02 -0400

    Create initial gameplay objects

---
+++ script.js
@@ -0,0 +1,19 @@
+const Gameboard = {
+    board: [['X','X','O']['O','O','X']['X','O','X']]
+}
+
+const playerOne = {
+    name: "Mikhail",
+    marker: "X"
+}
+
+const playerTwo = {
+    name: "Kelly",
+    marker: "O"
+}
+
+const playTurn = {
+    turn: function() {
+        // player selects a piece
+    }
+}
