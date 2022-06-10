3dfdbc3f498a412f2a240496c4d25c26075ee9f4
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2022-06-10 11:44:22 -0400

    Refactor by extracting functions

--- script.js
+++ script.js
@@ -4,9 +4,50 @@ const gameboard = (() => {
     return { board }
 })();
 
-const Player = (name, marker) => {
-    return { name, marker }
+function displayGameboard() {
+    let boardContainer = document.getElementById('gameboard');
+
+    for ( row = 0 ; row < gameboard.board.length ; row ++) {
+        displayRow = document.createElement('div');
+        displayRow.setAttribute('class', 'row');
+        displayCells(row)
+        boardContainer.appendChild(displayRow);
+    }
 };
+
+function displayCells(row) {
+    for ( cell = 0 ; cell < gameboard.board[row].length ; cell ++ ) {
+        displayCell = document.createElement('div');
+        displayCell.setAttribute('class', 'cell');
+        displayCell.setAttribute('id', `${row} ${cell}`);
+        displayCell.textContent = gameboard.board[row][cell];
+        displayRow.appendChild(displayCell);
+    }
+}
+
+function addEventListeners(marker) {
+    let allCells = document.getElementsByClassName('cell');
+    for (var i = 0 ; i < allCells.length; i++) {
+        allCells[i].addEventListener("click", function (e) {
+            let array = this.id.split(" ")
+            let row = parseInt(array[0])
+            let cell = parseInt(array[1])
+            gameboard.board[row][cell] = marker
+            nextPlayerTurn(marker)
+        })
+    }
+}
+
+function nextPlayerTurn(marker) {
+    clearGameboard()
+    displayGameboard()
+    if (marker == "X"){
+        addEventListeners("O")
+    } 
+    else {
+        addEventListeners("X")
+    }
+}
 
 function clearGameboard() {
     let boardContainer = document.getElementById('gameboard');
@@ -15,50 +56,13 @@ function clearGameboard() {
     }
 }
 
-function playTurn(marker) {
-    let allCells = document.getElementsByClassName('cell');
-    for (var i = 0 ; i < allCells.length; i++) {
-        allCells[i].addEventListener("click", function (e) {
-            let array = this.id.split(" ")
-            let row = parseInt(array[0])
-            let cell = parseInt(array[1])
-            gameboard.board[row][cell] = marker
-            clearGameboard()
-            displayGameboard()
-            if (marker == "X"){
-                playTurn("O")
-            } 
-            else {
-                playTurn("X")
-            }
-        })
-    }
-}
-
-function displayGameboard() {
-    let boardContainer = document.getElementById('gameboard');
-
-    let cellIndex = 0
-    for ( row = 0 ; row < gameboard.board.length ; row ++) {
-        displayRow = document.createElement('div');
-        displayRow.setAttribute('class', 'row');
-
-        for ( cell = 0 ; cell < gameboard.board[row].length ; cell ++ ) {
-            displayCell = document.createElement('div');
-            displayCell.setAttribute('class', 'cell');
-            displayCell.setAttribute('id', `${row} ${cell}`);
-            displayCell.textContent = gameboard.board[row][cell];
-            displayRow.appendChild(displayCell);
-            cellIndex += 1
-        }
-        
-        boardContainer.appendChild(displayRow);
-    }
+const Player = (name, marker) => {
+    return { name, marker }
 };
 
 const playGame = (() => {
     let playerOne = Player('Mikhail', 'X');
     let playerTwo = Player('Kelly', 'O');
     displayGameboard()
-    playTurn(playerOne.marker)
+    addEventListeners(playerOne.marker)
 })();
